f80cd81ef2a359c2f93d85cf6648bdfd37bec47e
sandramede <55195273+sandramede@users.noreply.github.com>
2020-06-07 13:06:06 +0300

    admin model and migration

---
+++ app/Admin.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App;
+
+use App\Notifications\ResetAdminPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+
+/**
+ * Class Admin
+ *
+ * @package App
+ * @property int $id
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static Builder|Admin newModelQuery()
+ * @method static Builder|Admin newQuery()
+ * @method static Builder|Admin query()
+ * @method static Builder|Admin whereCreatedAt($value)
+ * @method static Builder|Admin whereEmail($value)
+ * @method static Builder|Admin whereEmailVerifiedAt($value)
+ * @method static Builder|Admin whereFirstName($value)
+ * @method static Builder|Admin whereId($value)
+ * @method static Builder|Admin whereLastName($value)
+ * @method static Builder|Admin wherePassword($value)
+ * @method static Builder|Admin whereRememberToken($value)
+ * @method static Builder|Admin whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    /**
+     * @param string $token
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetAdminPasswordNotification($token));
+    }
+}
